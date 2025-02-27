66bacdb3b3b59a9d8be9fad4435bf09a278ec213
Nasser <youssef.nasser.ashour@gmail.com>
2025-02-27 14:50:35 +0200

    feat:Models and DB Context init (#1)

---
+++ Data/PizzaDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace PizzaApp.Models;
+public class PizzaDbContext : DbContext
+{
+	public PizzaDbContext(DbContextOptions<PizzaDbContext> options) : base(options) { }
+
+	public DbSet<Pizza> Pizzas { get; set; }
+	public DbSet<Order> Orders { get; set; }
+	public DbSet<OrderItem> OrderItems { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<OrderItem>()
+			.HasOne(oi => oi.Order)
+			.WithMany(o => o.OrderItems)
+			.HasForeignKey(oi => oi.OrderId);
+
+		modelBuilder.Entity<OrderItem>()
+			.HasOne(oi => oi.Pizza)
+			.WithMany()
+			.HasForeignKey(oi => oi.PizzaId);
+	}
+}
+
---
+++ Models/Pizza.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaApp.Models;
+
+public class Pizza
+{
+	public int PizzaId { get; set; }
+
+	[Required, MaxLength(100)]
+	public string Name { get; set; } = string.Empty;
+
+	public string Description { get; set; } = string.Empty;
+
+	[Required]
+	public decimal Price { get; set; }
+
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
+
