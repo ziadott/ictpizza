261e2577cdbbb10390df5400c90febc2fb8b96a5
ziadott <zeyadalkott@gmail.com>
2025-02-27 15:21:42 +0200

    added controllers

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ictpizza.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly PizzaDbContext _context;
+
+        public OrderController(PizzaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/order
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+            return await _context.Orders.Include(o => o.OrderItems).ThenInclude(oi => oi.Pizza).ToListAsync();
+        }
+
+        // GET: api/order/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+            var order = await _context.Orders.Include(o => o.OrderItems).ThenInclude(oi => oi.Pizza)
+                                             .FirstOrDefaultAsync(o => o.OrderId == id);
+            if (order == null)
+                return NotFound();
+
+            return order;
+        }
+
+        // POST: api/order
+        [HttpPost]
+        public async Task<ActionResult<Order>> CreateOrder(Order order)
+        {
+            // Calculate total price
+            order.TotalPrice = order.OrderItems.Sum(oi => oi.Subtotal);
+
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetOrder), new { id = order.OrderId }, order);
+        }
+
+        // PUT: api/order/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateOrder(int id, Order order)
+        {
+            if (id != order.OrderId)
+                return BadRequest();
+
+            _context.Entry(order).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/order/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+                return NotFound();
+
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ictpizza.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        private readonly PizzaDbContext _context;
+
+        public PizzaController(PizzaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/pizza
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pizza>>> GetPizzas()
+        {
+            return await _context.Pizzas.ToListAsync();
+        }
+
+        // GET: api/pizza/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pizza>> GetPizza(int id)
+        {
+            var pizza = await _context.Pizzas.FindAsync(id);
+            if (pizza == null)
+                return NotFound();
+
+            return pizza;
+        }
+
+        // POST: api/pizza
+        [HttpPost]
+        public async Task<ActionResult<Pizza>> CreatePizza(Pizza pizza)
+        {
+            _context.Pizzas.Add(pizza);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetPizza), new { id = pizza.PizzaId }, pizza);
+        }
+
+        // PUT: api/pizza/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePizza(int id, Pizza pizza)
+        {
+            if (id != pizza.PizzaId)
+                return BadRequest();
+
+            _context.Entry(pizza).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/pizza/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePizza(int id)
+        {
+            var pizza = await _context.Pizzas.FindAsync(id);
+            if (pizza == null)
+                return NotFound();
+
+            _context.Pizzas.Remove(pizza);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
